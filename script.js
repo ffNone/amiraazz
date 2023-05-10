7e8f2321e53af2a50efb782bdf63183c944e7c4c
unknown <uknown@ANONYMOUS@NONE.NULL.COM>
2023-05-10 13:17:02 +0300

    first-commit

---
+++ script.js
@@ -0,0 +1,26 @@
+let x = document.getElementById("inp")
+let y = document.getElementById("sub")
+let hola = document.getElementById("hola")
+let body = document.body
+hola.style.display = 'none'
+x.style.cssText = 'display:block; font-size : 30px; text-align : center'
+y.style.cssText = 'font-size : 30px; display: none'
+body.style.backgroundColor = 'Black'
+
+x.onclick = function() {
+    x.style.backgroundColor = 'yellow'
+    y.style.display = 'block'
+}
+y.onclick = function() {
+   let xx = x.value
+   window.alert(`اهلا ${xx} عاوز أقولك إني بحبك أوي 
+   ومنفسيش أبدا تكوني زعلانة
+   هفضل جمبك لآخر العمر وعايزك تكملي عشان خاطر ربنا وعشان خاطر نفسك
+   إوعديني إنك تكملي عشان أنا أأقدر أكمل ` , "أوعدك")
+   alert("بحبك")
+
+   x.remove()
+   y.remove()
+  hola.style.display = "block"
+  hola.style.cssText= 'padding-left : 50px; font-weight : bold; font-size: 50px'
+}
